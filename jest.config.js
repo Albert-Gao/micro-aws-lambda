5e477d6dcf303f31c96eb2bfb18b2457ab8eca7f
Albert Gao <albert.gao@lottonz.co.nz>
2020-02-15 14:12:49 +1300

    fix: added script

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   transform: {
     '.ts':
-      '/Users/AlbertGao/codes/teamingCloud/micro-lambda/node_modules/ts-jest/dist/index.js',
+      '/Users/AlbertGao/codes/teamingCloud/micro-aws-lambda/node_modules/ts-jest/dist/index.js',
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   collectCoverageFrom: ['src/**/*.ts'],
   testMatch: ['<rootDir>/**/*.(spec|test).ts'],
   testURL: 'http://localhost',
-  rootDir: '/Users/AlbertGao/codes/teamingCloud/micro-lambda',
+  rootDir: '/Users/AlbertGao/codes/teamingCloud/micro-aws-lambda',
   watchPlugins: [
-    '/Users/AlbertGao/codes/teamingCloud/micro-lambda/node_modules/jest-watch-typeahead/filename.js',
-    '/Users/AlbertGao/codes/teamingCloud/micro-lambda/node_modules/jest-watch-typeahead/testname.js',
+    '/Users/AlbertGao/codes/teamingCloud/micro-aws-lambda/node_modules/jest-watch-typeahead/filename.js',
+    '/Users/AlbertGao/codes/teamingCloud/micro-aws-lambda/node_modules/jest-watch-typeahead/testname.js',
   ],
 };
