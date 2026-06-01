0e9248c88b33dbea7a3b1a2bccbd16fb67b650ec
AlbertGao <albertgaohy@gmail.com>
2026-06-01 15:18:45 +0100

    test: scope coverage to source files

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
   watchman: false,
 };
