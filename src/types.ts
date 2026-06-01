d9e860cd6e010e5b2378f0b1078fee86c37daf08
AlbertGao <albertgaohy@gmail.com>
2026-06-01 14:04:24 +0100

    Fix public type API inconsistencies

--- src/types.ts
+++ src/types.ts
@@ -3,35 +3,11 @@ import {
   Context,
   APIGatewayProxyResult,
   APIGatewayProxyEventV2,
-  APIGatewayProxyResultV2,
+  APIGatewayProxyStructuredResultV2,
 } from 'aws-lambda';
 import { createTraceInfo } from './utils';
 
-export type IHttpResponse<ResponseDataType = any> = Omit<
-  APIGatewayProxyResultV2<ResponseDataType>,
-  'body'
-> & {
-  body: ResponseDataType & {
-    debug?: ReturnType<typeof createTraceInfo>;
-  };
-};
-
-export type Middleware<Shared = any, ResponseDataType = any> = ({
-  event,
-  context,
-  shared,
-}: {
-  event: APIGatewayProxyEventV2;
-  context: Context;
-  shared: Shared;
-}) =>
-  | IHttpResponse<ResponseDataType>
-  | Promise<ResponseDataType | null | undefined | void | Error>
-  | ResponseDataType
-  | null
-  | undefined
-  | void
-  | Error;
+type MaybePromise<T> = T | Promise<T>;
 
 export type IHttpResponseLegacy<ResponseDataType = any> = Omit<
   APIGatewayProxyResult,
@@ -42,19 +18,61 @@ export type IHttpResponseLegacy<ResponseDataType = any> = Omit<
   };
 };
 
-export type MiddlewareLegacy<Shared = any, ResponseDataType = any> = ({
-  event,
-  context,
-  shared,
-}: {
-  event: APIGatewayProxyEvent;
-  context: Context;
-  shared: Shared;
-}) =>
-  | IHttpResponseLegacy<ResponseDataType>
-  | Promise<ResponseDataType | null | undefined | void | Error>
+export type IHttpResponseV2<ResponseDataType = any> = Omit<
+  APIGatewayProxyStructuredResultV2,
+  'body'
+> & {
+  body: ResponseDataType & {
+    debug?: ReturnType<typeof createTraceInfo>;
+  };
+};
+
+export type IHttpResponse<ResponseDataType = any> = IHttpResponseLegacy<
+  ResponseDataType
+>;
+
+type SupportedApiGatewayEvent = APIGatewayProxyEvent | APIGatewayProxyEventV2;
+
+type HttpResponseForEvent<
+  Event extends SupportedApiGatewayEvent,
+  ResponseDataType
+> = Event extends APIGatewayProxyEventV2
+  ? IHttpResponseV2<ResponseDataType>
+  : IHttpResponseLegacy<ResponseDataType>;
+
+type MiddlewareResult<
+  Event extends SupportedApiGatewayEvent,
+  ResponseDataType
+> =
+  | HttpResponseForEvent<Event, ResponseDataType>
   | ResponseDataType
   | null
   | undefined
   | void
   | Error;
+
+export type Middleware<
+  Shared = any,
+  ResponseDataType = any,
+  Event extends SupportedApiGatewayEvent = APIGatewayProxyEvent
+> = ({
+  event,
+  context,
+  shared,
+}: {
+  event: Event;
+  context: Context;
+  shared: Shared;
+}) => MaybePromise<MiddlewareResult<Event, ResponseDataType>>;
+
+export type MiddlewareLegacy<Shared = any, ResponseDataType = any> = Middleware<
+  Shared,
+  ResponseDataType,
+  APIGatewayProxyEvent
+>;
+
+export type MiddlewareV2<Shared = any, ResponseDataType = any> = Middleware<
+  Shared,
+  ResponseDataType,
+  APIGatewayProxyEventV2
+>;
