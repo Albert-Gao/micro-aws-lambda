cf6063f41ac6fc5d0022bf8cc4bdddabd1b5cf7c
AlbertGao <albertgaohy@gmail.com>
2026-06-01 15:18:59 +0100

    test: enforce full coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   watchman: false,
 };
